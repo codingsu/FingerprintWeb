6942e1be62e857d30c33714242d808c58f604944
sujunwei <sjwv587@gmail.com>
2018-01-22 15:32:31 +0800

    修复翻页问题

--- page/config.py
+++
@@ -1,7 +0,0 @@
-dbip ='localhost'
-dbusername = 'root'
-dbpassword = 'wrjfaxcvmlkwr'
-dbname = 'pageweb'
-
-conferencefilepath = '/home/arkteam_paper/static/conference/'
-paperfilepath = '/home/arkteam_paper/static/paper/'
